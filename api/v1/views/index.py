64df1a32f18661f5aee56ceefacd07b7b571d488
namycodes <122107350+namycodes@users.noreply.github.com>
2024-08-05 02:14:53 +0200

    Finished task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,22 @@ create a route /status
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns a JSON response ok"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Returns stats(number of objects by type)"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats), 200
