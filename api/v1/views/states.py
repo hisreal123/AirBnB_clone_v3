54183c117bb6a1938143eb2c1e0b9f427dbb830d
hisreal123 <omosgoerge@gmail.com>
2024-08-10 12:02:08 +0100

    fix error handler

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,13 +3,17 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states',
+                 methods=['GET'], strict_slashes=False)
 def get_states():
     """Get all states."""
     states = [state.to_dict() for state in storage.all('State').values()]
     return jsonify(states)
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
     """Get state by id."""
     state = storage.get("State", state_id)
@@ -17,7 +21,9 @@ def get_state_by_id(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """Delete state by id."""
     state = storage.get("State", state_id)
@@ -27,7 +33,9 @@ def delete_state(state_id):
     state.save()
     return jsonify({})
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states', methods=['POST'],
+                 strict_slashes=False)
 def create_state():
     """Create a new state."""
     if not request.get_json():
@@ -38,7 +46,9 @@ def create_state():
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/states/<string:state_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Update state by id."""
     state = storage.get("State", state_id)
