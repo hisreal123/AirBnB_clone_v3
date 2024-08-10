93e8303dbd41619f59e4bce193570bc6f1a0e86e
hisreal123 <omosgoerge@gmail.com>
2024-08-10 09:58:30 +0100

    fix error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,10 @@
 Flask app for AirBnB
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,9 +17,9 @@ def teardown_appcontext(exception):
     storage.close()
 
 
-# 404 handler
 @app.errorhandler(404)
 def not_found(e):
+    """ error page handler"""
     return jsonify({"error": "Not found"})
 
 
